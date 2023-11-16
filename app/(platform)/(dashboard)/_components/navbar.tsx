68d78b224daa8bb015d8d77699094511f2253a74
minhng-178 <anhminh1782002@gmail.com>
2023-11-16 18:14:19 +0700

    sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,26 +1,32 @@
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Plus } from 'lucide-react';
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 
 import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
+// import { FormPopover } from "@/components/form/form-popover";
+
+import { MobileSidebar } from './mobile-sidebar';
+
 export const Navbar = () => {
   return (
-    <nav className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
         <Button
-          size={'sm'}
-          variant={'primary'}
-          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+          variant="primary"
+          size="sm"
+          className="rounded-sm hidden md:block h-auto  py-1.5 px-2"
         >
           Create
         </Button>
+
         <Button
-          size={'sm'}
-          variant={'primary'}
-          className="rounded-sm block md:hidden "
+          variant="primary"
+          size="sm"
+          className="rounded-sm block md:hidden"
         >
           <Plus className="h-4 w-4" />
         </Button>
@@ -28,15 +34,15 @@ export const Navbar = () => {
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
           hidePersonal
-          afterCreateOrganizationUrl={'organization/:id'}
-          afterSelectOrganizationUrl={'/organization/:id'}
+          afterCreateOrganizationUrl="/organization/:id"
           afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
           appearance={{
             elements: {
               rootBox: {
                 display: 'flex',
                 justifyContent: 'center',
-                alignContent: 'center',
+                alignItems: 'center',
               },
             },
           }}
