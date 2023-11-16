efa1fad40ed457c0f3cd1d28b854905079fe54a2
minhng-178 <anhminh1782002@gmail.com>
2023-11-16 21:09:48 +0700

    server actions

---
+++ app/(platform)/(dashboard)/organization/[organiztionId]/settings/page.tsx
@@ -0,0 +1,25 @@
+import { OrganizationProfile } from '@clerk/nextjs';
+
+const SettingPage = () => {
+  return (
+    <div>
+      <OrganizationProfile
+        appearance={{
+          elements: {
+            rootBox: {
+              boxShadow: 'none',
+              width: '100%',
+            },
+            card: {
+              border: '1px solid #e5e5e5',
+              boxShadow: 'none',
+              width: '100%',
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default SettingPage;
