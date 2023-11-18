09f78c5313b533417900132ecdd28b85a6843467
minhng-178 <anhminh1782002@gmail.com>
2023-11-18 10:02:25 +0700

    create board

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -10,23 +10,43 @@ import { InputType, ReturnType } from './types';
 import { CreateBoard } from './schema';
 
 const handler = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth();
+  const { userId, orgId } = auth();
 
-  if (!userId) {
+  if (!userId || !orgId) {
     return {
       error: 'Unauthorized',
     };
   }
 
-  const { title } = data;
+  const { title, image } = data;
+
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+    image.split('|');
+
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageUserName ||
+    !imageLinkHTML
+  ) {
+    return {
+      error: 'Missing fields. Failed to create board.',
+    };
+  }
 
   let board;
 
   try {
-    throw new Error('a');
     board = await db.board.create({
       data: {
         title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageLinkHTML,
+        imageUserName,
       },
     });
   } catch (error) {
