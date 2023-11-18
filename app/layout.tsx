0a8bfd9a34e504b1a01f7407915a152b9f5aa6d7
minhng-178 <anhminh1782002@gmail.com>
2023-11-18 21:13:47 +0700

    card form

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
+    template: `%s · ${siteConfig.name}`,
   },
   description: siteConfig.description,
   icons: [
