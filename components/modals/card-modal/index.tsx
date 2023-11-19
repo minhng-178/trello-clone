f221b205d594023b872f0768d210cbb66e60aa6e
minhng-178 <anhminh1782002@gmail.com>
2023-11-19 10:37:43 +0700

    activity| logs

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -4,12 +4,14 @@ import { useQuery } from '@tanstack/react-query';
 
 import { CardWithList } from '@/types';
 import { fetcher } from '@/lib/fetcher';
+import { AuditLog } from '@prisma/client';
 import { useCardModal } from '@/hooks/use-card-modal';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 
 import { Header } from './header';
 import { Description } from './description';
 import { Actions } from './actions';
+import { Activity } from './activity';
 
 export const CardModal = () => {
   const id = useCardModal(state => state.id);
@@ -21,11 +23,15 @@ export const CardModal = () => {
     queryFn: () => fetcher(`/api/cards/${id}`),
   });
 
+  const { data: auditLogsData } = useQuery<AuditLog[]>({
+    queryKey: ['card-logs', id],
+    queryFn: () => fetcher(`/api/cards/${id}/logs`),
+  });
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
-
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
           <div className="col-span-3">
             <div className="w-full space-y-6">
@@ -33,6 +39,11 @@ export const CardModal = () => {
                 <Description.Skeleton />
               ) : (
                 <Description data={cardData} />
+              )}
+              {!auditLogsData ? (
+                <Activity.Skeleton />
+              ) : (
+                <Activity items={auditLogsData} />
               )}
             </div>
           </div>
